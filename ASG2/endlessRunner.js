093e3d4326f9cc1c3c42dfb849361310bd001e69
Andy Espejo <Andy.Espejo5@gmail.com>
2016-04-18 16:41:27 -0700

    Basic particle system, needs major tweak

--- ASG2/endlessRunner.js
+++ ASG2/endlessRunner.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 obstacles = [];
 
-function Obstacle(lifeTime, speed, color, width, height, x, y) {
+function Obstacle(lifeTime, speed, color, width, height, x, y, period) {
     this.lifeTime = lifeTime;
     this.speed = speed;
     this.color = color;
@@ -10,48 +10,41 @@ function Obstacle(lifeTime, speed, color, width, height, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;
-
 }
 
 
 function obstacleSystem(numObstacles) {
     for(var i = 0; i < numObstacles; i++){
         obstacles.push(new Obstacle(Math.random()*10,Math.random()*10, "red",
-            10, 10, canvas.width - 100, Math.random()*canvas.height ))
+            10, 10, canvas.width, Math.random()*canvas.height ), 2)
     }
-
 }
-
-obstacleSystem(5);
-
-
-
-
-
-
-
-
-
-
-
+obstacleSystem(50);
 
 
 function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.width = canvas.width;
     context.fillStyle = 'blue';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     for(var i = 0; i < obstacles.length; i++){
         var obst  = obstacles[i];
-        context.fillStyle = obst.color;
+        context.fillStyle = "red";
         context.rect(obst.x, obst.y, obst.width, obst.height);
-        context.stroke();
+        context.fill();
     }
-    
-
 }
 
 function update() {
+    for(var i = 0; i < obstacles.length; i++){
+        if( obstacles[i].x > 0) {
+            obstacles[i].x -= obstacles[i].speed;
+        }else{
+            obstacles[i].x = canvas.width;
+            obstacles[i].lifeTime = canvas.width;
+            obstacles[i].y = Math.random()*canvas.height;
+        }
+    }
 
 }
 
