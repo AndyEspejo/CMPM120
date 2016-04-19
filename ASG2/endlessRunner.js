b6d20a6b067ec6b79a4b3044fe7fbcb619bea4c5
Andy <andy.espejo5@gmail.com>
2016-04-19 16:46:07 -0700

    Added fish object, need to add movement and then collision

--- ASG2/endlessRunner.js
+++ ASG2/endlessRunner.js
@@ -2,6 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 obstacles = [];
 
+//The obstacle object, can be used to produce different types of obstacles
 function Obstacle(lifeTime, speed, color, width, height, x, y, period) {
     this.lifeTime = lifeTime;
     this.speed = speed;
@@ -13,20 +14,42 @@ function Obstacle(lifeTime, speed, color, width, height, x, y, period) {
     this.period = period;
 }
 
+//Object for the playable fish
+function PlayerFish(src){
+    var hp = 100;
+    var image = new Image();
+    image.src = src;
+    
+    this.draw = function(x, y){
+	context.drawImage(image, x, y,);	
+    }
+    
+    this.update = function(){
+    	while(hp > 0){
+    	//Add movement
+    	}
+    }
+    
+}
+
+//Takes in a number and creates that number of obstacles, then puts it in the array
 function obstacleSystem(numObstacles) {
     for(var i = 0; i < numObstacles; i++){
         obstacles.push(new Obstacle(Math.random()*10, 15 - Math.random()*10, "red",
             10, 10, canvas.width, Math.random()*canvas.height,10 ))
     }
 }
+
 obstacleSystem(10);
+var player = new PlayerFish("http://people.ucsc.edu/~aespejo/120/ASG2/Fish2.png";
 
 
 function draw() {
     canvas.width = canvas.width;
     context.fillStyle = 'blue';
     context.fillRect(0, 0, canvas.width, canvas.height);
-
+    
+    player.draw(0,0);
     for(var i = 0; i < obstacles.length; i++){
         var obst  = obstacles[i];
         context.fillStyle = "red";
